6940755cc19636359d990aaf96bdaa7594143ab9
Lasse Wenger <lassewenger@gmail.com>
2016-11-29 10:50:39 +0100

    API GET metode from date tickets

--- src/main/java/rest/Flights.java
+++ src/main/java/rest/Flights.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
-import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +11,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
@@ -31,14 +26,9 @@ import javax.ws.rs.core.MediaType;
 import javax.xml.ws.http.HTTPException;
 import org.json.JSONObject;
 
-/**
- * REST Web Service
- *
- * @author Bruger
- */
 @Path("flights")
 public class Flights {
-    
+
     private static final String STARTURL = "http://airline-plaul.rhcloud.com/api";
 
     @Context
@@ -52,14 +42,43 @@ public class Flights {
 
     /**
      * Retrieves representation of an instance of rest.Flights
+     *
      * @return an instance of java.lang.String
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{from}/{date}/{tickets}")
+    public String flightsParam(@PathParam("from") String from, @PathParam("date") String date, @PathParam("tickets") String tickets) {
+
+        String dateString = date;
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+        Date dateFormat = format.parse(dateString);
+
+        // What is considered best: stringBuffer, or stringBuilder ?
+        StringBuilder result = new StringBuilder();
+        
+        URL requestURL = new URL(STARTURL + "/flightinfo/" + from + "/" + formatDate(d.getTime()) + "/" + tickets);
+        InputStream in = requestURL.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        JSONObject o = new JSONObject(result.toString());
+        return o.toString(2);
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("getflights")
     public String getFlights() {
-        
-            StringBuilder result = new StringBuilder();
+
+        // What is considered best: stringBuffer, or stringBuilder ?
+        StringBuilder result = new StringBuilder();
+//            
         try {
             URL url = new URL(STARTURL + "/flightinfo/CPH/2017-01-20T00:00:00.000Z/1");
             InputStream in = url.openStream();
@@ -67,43 +86,46 @@ public class Flights {
             String line;
             while ((line = reader.readLine()) != null) {
                 result.append(line);
+//                
             }
             System.out.println(result.toString());
-            
+
         } catch (IOException ex) {
             Logger.getLogger(Flights.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return new Gson().toJson(result);
-        
+        JSONObject o = new JSONObject(result.toString());
+        return o.toString(2);
+//        return new Gson().toJson(result.toString());
+//return result.toString();
+
     }
-    
+
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{from}/{date}/{ticket}")
-    public String getFlightFromDate(@PathParam("from") String from, @PathParam("date") String date, @PathParam("ticket") String ticket){
-        
+    public String getFlightFromDate(@PathParam("from") String from, @PathParam("date") String date, @PathParam("ticket") String ticket) {
+
         try {
             String[] array = date.split("-");
             Calendar c = Calendar.getInstance();
-            
+
             int year = Integer.parseInt(array[0]);
             int month = Integer.parseInt(array[1]);
             int day = Integer.parseInt(array[2]);
-            
+
             c.set(year, month - 1, day);
-            
+
             String requestURL = STARTURL + "/flightinfo/" + from + "/" + formatDate(c.getTime()) + "/" + ticket;
-            
+
             System.out.println("From " + from); // skal ikke converteres
             System.out.println("Array index 0 " + year);
             System.out.println("Array index 1 " + month);
             System.out.println("Array index 2 " + day);
             System.out.println("Date Object " + c.getTime());
-            
+
             String response;
-            
+
             response = sendGet(requestURL);
             JSONObject o = new JSONObject(response);
 
@@ -118,21 +140,20 @@ public class Flights {
 
     /**
      * PUT method for updating or creating an instance of Flights
+     *
      * @param content representation for the resource
      */
     @PUT
     @Consumes(MediaType.APPLICATION_XML)
     public void putXml(String content) {
     }
-    
-    private String formatDate(Date date)
-    {
+
+    private String formatDate(Date date) {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         return df.format(date);
     }
-    
-    private String sendGet(String requestURL) throws MalformedURLException, IOException, HTTPException
-    {
+
+    private String sendGet(String requestURL) throws MalformedURLException, IOException, HTTPException {
         String response;
         URL url = new URL(requestURL);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -140,25 +161,21 @@ public class Flights {
         conn.setRequestMethod("GET");
 
         int responseCode = conn.getResponseCode();
-        if (responseCode == 200)
-        {
+        if (responseCode == 200) {
             response = getResponseBody(conn.getInputStream());
-        } else
-        {
+        } else {
             response = getResponseBody(conn.getErrorStream());
             //throw new HTTPException(conn.getResponseCode()+" "+conn.getResponseMessage());
         }
         return response;
     }
-    
-    private String getResponseBody(InputStream is) throws IOException
-    {
+
+    private String getResponseBody(InputStream is) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(is));
         String inputLine;
         StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null)
-        {
+        while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
