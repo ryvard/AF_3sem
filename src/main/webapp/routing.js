45214c0c7d26b561199fec252623c4c1ffaee8cd
eblomst <emma.blomsterberg@hotmail.com>
2016-11-30 11:02:15 +0100

    Moving search bar ng-show

--- src/main/webapp/routing.js
+++ src/main/webapp/routing.js
@@ -1,23 +1,27 @@
 'use strict';
-angular.module('routingModule', ['ngRoute','resultModule'])
+angular.module('routingModule', ['ngRoute', 'resultModule'])
 
-.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-                .when('/', {
-                    templateUrl: 'home.html'
-                })
-                .when('/search', {
-                    templateUrl: 'searchview/searchview.html',
-                    controller: 'searchCntr'
-                })
-                .when('/reserve', {
-                    templateUrl: 'passengerview/passengerview.html',
-                    controller: 'searchCntr'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
-    }]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider
+                        .when('/', {
+                            templateUrl: 'home.html'
+                        })
+                        .when('/search', {
+                            templateUrl: 'searchview/searchview.html',
+                            controller: 'searchCntr'
+                        })
+                        .when('/reserve', {
+                            templateUrl: 'passengerview/passengerview.html',
+                            controller: 'searchCntr'
+                        })
+                        .when('/confirm', {
+                            templateUrl: 'confirmationview/confirmationview.html'
+
+                        })
+                        .otherwise({
+                            redirectTo: '/'
+                        });
+            }]);
 
 
 
