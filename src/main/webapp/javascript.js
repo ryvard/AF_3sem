537d6ba5978396b2928b125742b9fe412e5f13fe
Mia Ryvard <cph-mr279@cphbusiness.dk>
2016-12-01 14:13:00 +0100

    hej

--- src/main/webapp/javascript.js
+++ src/main/webapp/javascript.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('resultModule', [])
-   
+
 
 //        .controller('searchBarCntr', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 //
@@ -48,23 +48,41 @@ angular.module('resultModule', [])
                 $scope.date;
                 $scope.tickets;
                 $scope.flightInfo;
-
+                $scope.iataCodes;
                 $scope.departAirport = 'CPH';
+                $scope.arrivalAirport = 'all destinations';
                 $scope.date = '2017-01-18';
                 $scope.tickets = 1;
-
                 $scope.getFlights = function () {
                     console.log('GET FLIGHTS');
-                    $http.get('http://localhost:8080/AF_3sem/api/flights/'
-                            + $scope.departAirport + '/' + $scope.date + '/' + $scope.tickets).then(function (response) {
-                        $scope.airlineInfo = response.data;
+                    if ($scope.arrivalAirport === 'all destinations')
+                    {
+                        $http.get('http://localhost:8080/AF_3sem/api/flights/'
+                                + $scope.departAirport + '/' + $scope.date + '/' + $scope.tickets).then(function (response) {
+                            $scope.airlineInfo = response.data;
 //                        dataFactory.set($scope.airlineInfo);
-                        console.log(response.data);
-                        console.log($scope.airlineInfo.airline);
+                            console.log(response.data);
+                            console.log($scope.airlineInfo.airline);
+                        }, function (error) {
+                        });
+                    } else {
+                        $http.get('http://localhost:8080/AF_3sem/api/flights/'
+                                + $scope.departAirport + '/' + $scope.arrivalAirport + '/' + $scope.date + '/' + $scope.tickets).then(function (response) {
+                            $scope.airlineInfo = response.data;
+//                        dataFactory.set($scope.airlineInfo);
+                            console.log(response.data);
+                            console.log($scope.airlineInfo.airline);
+                        }, function (error) {
+                        });
+                    }
+                    ;
+                    $http.get('https://iatacodes.org/api/v6/airports?api_key=8a2623ff-1ca6-4250-aa10-838fb259775a').then(function (response) {
+                        $scope.iataCodes = response.data;
+                        console.log($scope.iataCodes);
                     }, function (error) {
-                    });
-                };
 
+                    });
+                }
             }])
 
 //        .factory('dataFactory', ['$scope', function($scope) {
