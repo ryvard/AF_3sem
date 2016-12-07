03d4776b28bf7e566f9c9ea66a6d9a1ba02eb6ca
Lasse Wenger <lassewenger@gmail.com>
2016-12-07 11:40:48 +0100

    Lavet Factory, dele data mellem views

--- src/main/webapp/javascript.js
+++ src/main/webapp/javascript.js
@@ -3,7 +3,7 @@
 angular.module('resultModule', [])
 
         .controller('searchCntr', ['$scope', '$http', 'flights', function ($scope, $http, flights) {
-                
+
                 $scope.airlineInfo;
                 $scope.departAirport;
                 $scope.arrivalAirport;
@@ -11,76 +11,45 @@ angular.module('resultModule', [])
                 $scope.tickets;
                 $scope.flightInfo;
                 $scope.iataCodes;
+                $scope.departAirport = flights.inputs.depart;
+                $scope.arrivalAirport = flights.inputs.arrival;
+                $scope.date = flights.inputs.date;
+                $scope.tickets = flights.inputs.ticket;
+                $scope.addInputs = function (depart, arrival, date, ticket) {
+                    flights.addInput(depart, arrival, date, ticket);
+                };
                 
-//                ------------------------------------------------
-//                $scope.list = flights.list.tickets;
-              
-//              ---------------------------------------------------
-//                $scope.departAirport = 'CPH';
-                $scope.departAirport  = flights.list.tickets;
-                $scope.arrivalAirport = 'all destinations';
-                $scope.date = '2017-01-18';
-                $scope.tickets = 1;
-//                $scope.getFlights();
-                $scope.getScope = function () {
-                    console.log('-----------------------------------------------');
-                    console.log($scope);
-                    console.log('-----------------------------------------------');
-                };
-                  $scope.addTicket = function (input) {
-                    flights.addInput(input);
-                };
                 $scope.getFlights = function () {
-                    console.log('GET FLIGHTS');
                     if ($scope.arrivalAirport === 'all destinations')
                     {
                         $http.get('http://localhost:8080/AF_3sem/api/flights/'
                                 + $scope.departAirport + '/' + $scope.date + '/' + $scope.tickets).then(function (response) {
                             $scope.airlineInfo = response.data;
-//                        dataFactory.set($scope.airlineInfo);
-                            console.log(response.data);
-                            console.log($scope.airlineInfo.airline);
-                            console.log($scope);
                         }, function (error) {
                         });
                     } else {
                         $http.get('http://localhost:8080/AF_3sem/api/flights/'
                                 + $scope.departAirport + '/' + $scope.arrivalAirport + '/' + $scope.date + '/' + $scope.tickets).then(function (response) {
                             $scope.airlineInfo = response.data;
-//                        dataFactory.set($scope.airlineInfo);
-                            console.log(response.data);
-                            console.log($scope.airlineInfo.airline);
                         }, function (error) {
                         });
                     }
                     ;
-//                    $http.get('https://iatacodes.org/api/v6/airports?api_key=8a2623ff-1ca6-4250-aa10-838fb259775a').then(function (response) {
-//                        $scope.iataCodes = response.data;
-//                        console.log($scope.iataCodes);
-//                    }, function (error) {
-//
-//                    });
-                   
                 };
                 $scope.getFlights();
-
             }])
-
 
         .factory('flights', [function () {
                 var flights = {};
 
-                flights.list = {};
-                
-                flights.addInput = function (input){
-                  flights.list = {tickets: input};  
+                flights.inputs = {};
+
+                flights.addInput = function (depart, arrival, date, ticket) {
+                    flights.inputs = {depart: depart, arrival: arrival, date: date, ticket: ticket};
                 };
 
                 return flights;
-
             }])
-
-
 
         .filter('durationFilter', function () {
             return function (time) {
@@ -93,7 +62,13 @@ angular.module('resultModule', [])
                 return (parseInt(timeSplit[0]) + (duration / 60 | 0)) + ":" + (parseInt(timeSplit[1]) + duration % 60);
             };
         });
-
-
-
-
+        
+        
+        
+        
+//                    $http.get('https://iatacodes.org/api/v6/airports?api_key=8a2623ff-1ca6-4250-aa10-838fb259775a').then(function (response) {
+//                        $scope.iataCodes = response.data;
+//                        console.log($scope.iataCodes);
+//                    }, function (error) {
+//
+//                    });
